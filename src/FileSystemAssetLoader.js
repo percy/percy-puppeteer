7343fdd6e5ac804f6e4794c0fd468f42ebf6bd38
Tim Haines <tmhaines@gmail.com>
2018-04-21 11:28:22 -0700

    Setup linting and reformat code

--- src/FileSystemAssetLoader.js
+++ src/FileSystemAssetLoader.js
@@ -12,94 +12,85 @@ const DEFAULT_SKIPPED_ASSETS = [];
  * Loader for assets that read from the file system.
  */
 class FileSystemAssetLoader {
-    constructor(options: {
-        buildDir: string,
-        mountPath?: string,
-        skippedAssets?: string[]
-    }) {
-        this.options = options;
-        this.options.skippedAssets =
-            this.options.skippedAssets || DEFAULT_SKIPPED_ASSETS;
-    }
+  constructor(options: { buildDir: string, mountPath?: string, skippedAssets?: string[] }) {
+    this.options = options;
+    this.options.skippedAssets = this.options.skippedAssets || DEFAULT_SKIPPED_ASSETS;
+  }
 
-    /*
+  /*
      * Find all resources for a build.
      */
-    async findBuildResources(
-        percyClient: PercyClient
-    ): Promise<PercyResource[]> {
-        const options = this.options;
-        const buildDir = options.buildDir;
+  async findBuildResources(percyClient: PercyClient): Promise<PercyResource[]> {
+    const options = this.options;
+    const buildDir = options.buildDir;
 
-        let mountPath = `${options.mountPath || ''}`;
+    let mountPath = `${options.mountPath || ''}`;
 
-        // Only add a / to the mountPath if it doesn't already end in one.
-        if (mountPath.slice(-1) != '/') {
-            mountPath = `${mountPath}/`;
-        }
-
-        const isDirectory = fs.statSync(buildDir).isDirectory();
-
-        if (!isDirectory) {
-            throw new Error(`${buildDir} is not a directory`);
-        }
-
-        const resources = [];
-        let errors;
-
-        walk.walkSync(buildDir, {
-            followLinks: true,
-            listeners: {
-                file: (root, fileStats, next) => {
-                    const absolutePath = path.join(root, fileStats.name);
-                    let resourceUrl = absolutePath;
-                    if (path.sep === '\\') {
-                        // Windows: transform filesystem backslashes into forward-slashes for the URL.
-                        resourceUrl = resourceUrl.replace(/\\/g, '/');
-                    }
-
-                    resourceUrl = resourceUrl.replace(buildDir, '');
-
-                    if (resourceUrl.charAt(0) === '/') {
-                        resourceUrl = resourceUrl.substr(1);
-                    }
-
-                    for (const assetPattern of options.skippedAssets) {
-                        if (resourceUrl.match(assetPattern)) {
-                            next();
-                            return;
-                        }
-                    }
-                    if (fs.statSync(absolutePath).size > MAX_FILE_SIZE_BYTES) {
-                        return;
-                    }
-
-                    const content = fs.readFileSync(absolutePath);
-                    resources.push(
-                        percyClient.makeResource({
-                            resourceUrl: encodeURI(
-                                `${mountPath}${resourceUrl}`
-                            ),
-                            content,
-                            mimetype: mime.lookup(resourceUrl)
-                        })
-                    );
-                    next();
-                },
-
-                errors: (root, fileStats, next) => {
-                    errors = fileStats;
-                    next();
-                }
-            }
-        });
-
-        if (resources.length === 0 && errors) {
-            throw errors;
-        }
-
-        return resources;
+    // Only add a / to the mountPath if it doesn't already end in one.
+    if (mountPath.slice(-1) != '/') {
+      mountPath = `${mountPath}/`;
     }
+
+    const isDirectory = fs.statSync(buildDir).isDirectory();
+
+    if (!isDirectory) {
+      throw new Error(`${buildDir} is not a directory`);
+    }
+
+    const resources = [];
+    let errors;
+
+    walk.walkSync(buildDir, {
+      followLinks: true,
+      listeners: {
+        file: (root, fileStats, next) => {
+          const absolutePath = path.join(root, fileStats.name);
+          let resourceUrl = absolutePath;
+          if (path.sep === '\\') {
+            // Windows: transform filesystem backslashes into forward-slashes for the URL.
+            resourceUrl = resourceUrl.replace(/\\/g, '/');
+          }
+
+          resourceUrl = resourceUrl.replace(buildDir, '');
+
+          if (resourceUrl.charAt(0) === '/') {
+            resourceUrl = resourceUrl.substr(1);
+          }
+
+          for (const assetPattern of options.skippedAssets) {
+            if (resourceUrl.match(assetPattern)) {
+              next();
+              return;
+            }
+          }
+          if (fs.statSync(absolutePath).size > MAX_FILE_SIZE_BYTES) {
+            return;
+          }
+
+          const content = fs.readFileSync(absolutePath);
+          resources.push(
+            percyClient.makeResource({
+              resourceUrl: encodeURI(`${mountPath}${resourceUrl}`),
+              content,
+              mimetype: mime.lookup(resourceUrl),
+            }),
+          );
+          next();
+        },
+
+        errors: (root, fileStats, next) => {
+          errors = fileStats;
+          next();
+        },
+      },
+    });
+
+    if (resources.length === 0 && errors) {
+      throw errors;
+    }
+
+    return resources;
+  }
 }
 
 export default FileSystemAssetLoader;
--- src/Percy.js
+++ src/Percy.js
@@ -9,141 +9,127 @@ import type FileSystemAssetLoader from './FileSystemAssetLoader';
  * Interface betwen the puppeteer browser and the percy API.
  */
 class Percy {
-    client: PercyClient;
-    environment: PercyEnvironment;
-    buildId: ?string;
-    loaders: FileSystemAssetLoader[];
+  client: PercyClient;
+  environment: PercyEnvironment;
+  buildId: ?string;
+  loaders: FileSystemAssetLoader[];
 
-    constructor({ loaders }: { loaders: FileSystemAssetLoader[] }) {
-        const token = process.env.PERCY_TOKEN;
-        const apiUrl = process.env.PERCY_API;
-        const clientInfo = `@percy/puppeteer ${packageJSON.version}`;
+  constructor({ loaders }: { loaders: FileSystemAssetLoader[] }) {
+    const token = process.env.PERCY_TOKEN;
+    const apiUrl = process.env.PERCY_API;
+    const clientInfo = `@percy/puppeteer ${packageJSON.version}`;
 
-        this.client = new PercyClient({ token, apiUrl, clientInfo });
-        this.environment = new PercyEnvironment(process.env);
-        this.loaders = loaders;
-    }
+    this.client = new PercyClient({ token, apiUrl, clientInfo });
+    this.environment = new PercyEnvironment(process.env);
+    this.loaders = loaders;
+  }
 
-    /*
+  /*
      * Take a screenshot of current browser.
      */
-    async snapshot(
-        name: string,
-        page: puppeteer.Page,
-        options: {
-            widths?: string[],
-            enableJavaScript?: boolean,
-            minimumHeight?: number
-        } = {}
-    ): Promise<void> {
-        if (!this.buildId) {
-            return;
-        }
-
-        const source = await page.content();
-
-        const rootResource = this.client.makeResource({
-            resourceUrl: page.url(),
-            content: source,
-            isRoot: true,
-            mimetype: 'text/html'
-        });
-
-        const snapshotResponse = await this.client.createSnapshot(
-            this.buildId,
-            [rootResource],
-            {
-                name,
-                widths: options.widths,
-                enableJavaScript: options.enableJavaScript,
-                minimumHeight: options.minimumHeight
-            }
-        );
-
-        const snapshotId = snapshotResponse.body.data.id;
-        const shaToResource = {};
-        shaToResource[rootResource.sha] = rootResource;
-
-        await this.uploadMissingResources(snapshotResponse, shaToResource);
-        await this.client.finalizeSnapshot(snapshotId);
+  async snapshot(
+    name: string,
+    page: puppeteer.Page,
+    options: {
+      widths?: string[],
+      enableJavaScript?: boolean,
+      minimumHeight?: number,
+    } = {},
+  ): Promise<void> {
+    if (!this.buildId) {
+      return;
     }
 
-    /*
+    const source = await page.content();
+
+    const rootResource = this.client.makeResource({
+      resourceUrl: page.url(),
+      content: source,
+      isRoot: true,
+      mimetype: 'text/html',
+    });
+
+    const snapshotResponse = await this.client.createSnapshot(this.buildId, [rootResource], {
+      name,
+      widths: options.widths,
+      enableJavaScript: options.enableJavaScript,
+      minimumHeight: options.minimumHeight,
+    });
+
+    const snapshotId = snapshotResponse.body.data.id;
+    const shaToResource = {};
+    shaToResource[rootResource.sha] = rootResource;
+
+    await this.uploadMissingResources(snapshotResponse, shaToResource);
+    await this.client.finalizeSnapshot(snapshotId);
+  }
+
+  /*
      * Start a new build.
      */
-    async startBuild(): Promise<void> {
-        if (this.buildId) {
-            throw new Error(
-                'There is already an active build, call percy.finalizeBuild() first'
-            );
-        }
-
-        // List all assets
-        const resources = await this.gatherBuildResources();
-
-        // Create the build
-        const buildResponse = await this.client.createBuild(
-            this.environment.repo,
-            { resources }
-        );
-        this.buildId = buildResponse.body.data.id;
-
-        // Upload the resources that do not exist in Percy
-        const shaToResource = {};
-        for (const resource of resources) {
-            shaToResource[resource.sha] = resource;
-        }
-
-        await this.uploadMissingResources(buildResponse, shaToResource);
+  async startBuild(): Promise<void> {
+    if (this.buildId) {
+      throw new Error('There is already an active build, call percy.finalizeBuild() first');
     }
 
-    /*
+    // List all assets
+    const resources = await this.gatherBuildResources();
+
+    // Create the build
+    const buildResponse = await this.client.createBuild(this.environment.repo, { resources });
+    this.buildId = buildResponse.body.data.id;
+
+    // Upload the resources that do not exist in Percy
+    const shaToResource = {};
+    for (const resource of resources) {
+      shaToResource[resource.sha] = resource;
+    }
+
+    await this.uploadMissingResources(buildResponse, shaToResource);
+  }
+
+  /*
      * Commit the build as finalized.
      */
-    async finalizeBuild(): Promise<void> {
-        if (!this.buildId) {
-            throw new Error(
-                'No build started, call percy.startBuild() first'
-            );
-        }
-
-        await this.client.finalizeBuild(this.buildId);
-        this.buildId = null;
+  async finalizeBuild(): Promise<void> {
+    if (!this.buildId) {
+      throw new Error('No build started, call percy.startBuild() first');
     }
 
-    /*
+    await this.client.finalizeBuild(this.buildId);
+    this.buildId = null;
+  }
+
+  /*
      * List all assets.
      */
-    async gatherBuildResources(): Promise<PercyResource[]> {
-        const listOfResources = await Promise.all(
-            this.loaders.map(loader => loader.findBuildResources(this.client))
-        );
+  async gatherBuildResources(): Promise<PercyResource[]> {
+    const listOfResources = await Promise.all(
+      this.loaders.map(loader => loader.findBuildResources(this.client)),
+    );
 
-        return [].concat(...listOfResources);
-    }
+    return [].concat(...listOfResources);
+  }
 
-    /*
+  /*
      * Upload missing resources.
      */
-    async uploadMissingResources(
-        response: *,
-        shaToResource: { [string]: PercyResource }
-    ): Promise<void> {
-        const missingResources = parseMissingResources(response);
-        const promises = [];
-        if (missingResources.length > 0) {
-            for (const missingResource of missingResources) {
-                promises.push(
-                    this.client.uploadResource(
-                        this.buildId,
-                        shaToResource[missingResource.id].content
-                    )
-                );
-            }
-        }
-
-        await Promise.all(promises);
+  async uploadMissingResources(
+    response: *,
+    shaToResource: { [string]: PercyResource },
+  ): Promise<void> {
+    const missingResources = parseMissingResources(response);
+    const promises = [];
+    if (missingResources.length > 0) {
+      for (const missingResource of missingResources) {
+        promises.push(
+          this.client.uploadResource(this.buildId, shaToResource[missingResource.id].content),
+        );
+      }
     }
+
+    await Promise.all(promises);
+  }
 }
 
 /*
@@ -151,13 +137,13 @@ class Percy {
  * percy.createBuild and percy.createSnapshot.
  */
 function parseMissingResources(response) {
-    return (
-        (response.body.data &&
-            response.body.data.relationships &&
-            response.body.data.relationships['missing-resources'] &&
-            response.body.data.relationships['missing-resources'].data) ||
-        []
-    );
+  return (
+    (response.body.data &&
+      response.body.data.relationships &&
+      response.body.data.relationships['missing-resources'] &&
+      response.body.data.relationships['missing-resources'].data) ||
+    []
+  );
 }
 
 export default Percy;
