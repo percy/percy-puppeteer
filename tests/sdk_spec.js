ebe391809b1e30ec21f74e2f6eabd99de6ac43e8
Ana Ulin <ana@ulin.org>
2018-12-06 15:27:07 -0800

    Add SDK tests and CircleCI integration.

---
+++ tests/sdk_spec.js
@@ -0,0 +1,96 @@
+const should = require('chai').should()
+const puppeteer = require('puppeteer')
+const httpServer = require('http-server')
+const { percySnapshot } = require('../dist')
+
+describe('@percy/puppeteer SDK', function() {
+  const PORT = 8000
+  const TEST_URL = `http://localhost:${PORT}`
+
+  let server = null
+  let browser = null
+  let page = null
+
+  before(async function() {
+    // Start local server to host app under test.
+    server = httpServer.createServer({ root: `${__dirname}/testapp` })
+    server.listen(PORT)
+
+    // Create a new Puppeteer browser instance.
+    browser = await puppeteer.launch({
+      headless: true,
+      timeout: 10000,
+      ignoreHTTPSErrors: true
+    })
+    page = await browser.newPage()
+    await page.setBypassCSP(true)
+  })
+
+  after(async function() {
+    // Close the Puppeteer session.
+    browser.close()
+
+    // Shut down the HTTP server.
+    server.close()
+  })
+
+  describe('with local app', async function() {
+    beforeEach(async function() {
+      await page.goto(TEST_URL)
+    })
+
+    afterEach(async function() {
+      // Clear local storage to start always with a clean slate.
+      await page.evaluate(() => localStorage.clear())
+    })
+
+    it('snapshots with provided name', async function() {
+      await percySnapshot(page, this.test.fullTitle())
+    })
+
+    it('snapshots with provided name and widths', async function() {
+      await percySnapshot(page, this.test.fullTitle(), {
+        widths: [768, 992, 1200],
+      })
+    })
+
+    it('snapshots with minHeight', async function() {
+      await percySnapshot(page, this.test.fullTitle(), { minHeight: 2000 })
+    })
+
+    it('takes multiple snapshots in one test', async function() {
+      // Add a todo.
+      await page.type('.new-todo', 'A thing to accomplish')
+      await page.keyboard.press('Enter')
+      let itemsLeft = await page.evaluate(
+        () => document.querySelector('.todo-count').textContent
+      )
+      itemsLeft.should.eq('1 item left')
+      await percySnapshot(page, `${this.test.fullTitle()} #1`)
+
+      await page.click('input.toggle')
+      itemsLeft = await page.evaluate(
+        () => document.querySelector('.todo-count').textContent
+      )
+      itemsLeft.should.eq('0 items left')
+      await percySnapshot(page, `${this.test.fullTitle()} #2`)
+    })
+  })
+
+  describe('with live sites', async function() {
+
+    it('snapshots HTTPS website', async function() {
+      await page.goto('https://polaris.shopify.com/')
+      await percySnapshot(page, this.test.fullTitle(), {
+        widths: [768, 992, 1200],
+      })
+    })
+
+    it('snapshots website with strict CSP', async function() {
+      await page.goto('https://buildkite.com/')
+      await percySnapshot(page, this.test.fullTitle(), {
+        widths: [768, 992, 1200],
+      })
+    })
+  })
+})
