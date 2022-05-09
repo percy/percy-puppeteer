5bb0ee094ffa6a5a7cda4b43668d1539ddb3fb64
Robert DeLuca <robertdeluca19@gmail.com>
2022-05-09 12:58:02 -0500

    🐛 Remove Dynamic import now that `@percy/sdk-utils` is commonjs
    We convereted `@percy/sdk-utils` to be ESM for 1.x, and this causes a lot of
    various issues. That has since been reverted (it's back to commonjs) so we can
    remove the dynamic import to fully remove any issues with ESM.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const utils = require('@percy/sdk-utils');
+
 // Collect client and environment information
 const sdkPkg = require('./package.json');
 const puppeteerPkg = require('puppeteer/package.json');
@@ -6,8 +8,6 @@ const ENV_INFO = `${puppeteerPkg.name}/${puppeteerPkg.version}`;
 
 // Take a DOM snapshot and post it to the snapshot endpoint
 async function percySnapshot(page, name, options) {
-  let utils = await import('@percy/sdk-utils');
-
   if (!page) throw new Error('A Puppeteer `page` object is required.');
   if (!name) throw new Error('The `name` argument is required.');
   if (!(await utils.isPercyEnabled())) return;
