8436150f8cd13cb8afb70553129c7a110418c239
Robert DeLuca <robertdeluca19@gmail.com>
2022-09-19 16:20:07 -0500

    ⬆️ Upgrade to the latest CLI

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -4,7 +4,17 @@ import helpers from '@percy/sdk-utils/test/helpers';
 import percySnapshot from '../index.js';
 
 describe('percySnapshot', () => {
-  let browser, page;
+  let browser, page, stdout, stderr;
+
+  let ANSI_REG = new RegExp('[\\u001B\\u009B][[\\]()#;?]*(' +
+    '(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|' +
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))', 'g');
+
+  let captureLogs = acc => msg => {
+    msg = msg.replace(/\r\n/g, '\n');
+    msg = msg.replace(ANSI_REG, '');
+    acc.push(msg.replace(/\n$/, ''));
+  };
 
   beforeAll(async function() {
     browser = await puppeteer.launch();
@@ -15,6 +25,12 @@ describe('percySnapshot', () => {
   });
 
   beforeEach(async () => {
+    stdout = [];
+    stderr = [];
+
+    spyOn(process.stdout, 'write').and.callFake(captureLogs(stdout));
+    spyOn(process.stderr, 'write').and.callFake(captureLogs(stderr));
+
     await helpers.setupTest();
     page = await browser.newPage();
     await page.goto(helpers.testSnapshotURL);
@@ -35,9 +51,8 @@ describe('percySnapshot', () => {
 
     await percySnapshot(page, 'Snapshot 1');
     await percySnapshot(page, 'Snapshot 2');
-
-    expect(await helpers.get('logs')).toEqual(jasmine.arrayContaining([
-      'Percy is not running, disabling snapshots'
+    expect(stdout).toEqual(jasmine.arrayContaining([
+      '[percy] Percy is not running, disabling snapshots'
     ]));
   });
 
@@ -59,8 +74,8 @@ describe('percySnapshot', () => {
 
     await percySnapshot(page, 'Snapshot 1');
 
-    expect(await helpers.get('logs')).toEqual(jasmine.arrayContaining([
-      'Could not take DOM snapshot "Snapshot 1"'
+    expect(stderr).toEqual(jasmine.arrayContaining([
+      '[percy] Could not take DOM snapshot "Snapshot 1"'
     ]));
   });
 });
